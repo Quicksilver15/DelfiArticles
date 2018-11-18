434ef226ab92871d1d6d8fadc8d97d5239ddb82c
Andrejs Skripnicenko <andy_miller@inbox.lv>
2018-11-18 22:07:48 +0200

    Update DelfiArticles.java

--- src/test/java/DelfiArticles.java
+++ src/test/java/DelfiArticles.java
@@ -5,50 +5,53 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class DelfiArticles {
-    private final By ARTICLE = By.xpath(".//a[@class = 'top2012-title']");
+public class DelfiArticleTest3 {
+    private final By WEB_ARTICLE = By.xpath(".//a[@class = 'text-mine-shaft']");
     private final By MOB_ARTICLE = By.xpath(".//a[@class = 'md-scrollpos']");
     private WebDriver browser;
+    private final String HOME_PAGE = "http://rus.delfi.lv";
+    private final String MOB_HOME_PAGE = "http://m.rus.delfi.lv";
 
     @Test
-    public void compareArticles(){
+    public void compareArticles() {
+        List<String> articleTitle = new ArrayList<String>();
+        articleTitle.add("Прямой эфир: На Набережной в ожидании салюта собрались десятки тысяч человек");
+        articleTitle.add("ВИДЕО: военный парад, посвященный 100-летию провозглашения независимости Латвии");
+        articleTitle.add("Грибаускайте не смогла вылететь в Ригу на 100-летие независимости Латвии");
+        articleTitle.add("Вейонис: мы готовы защищать свою страну и народ");
+        articleTitle.add("ФОТО, ВИДЕО: В центре Риги прошло грандиозное факельное шествие");
+
         System.setProperty("webdriver.chrome.driver", "c:/chromedriver.exe");
         browser = new ChromeDriver();
         browser.manage().window().maximize();
-        browser.get("http://rus.delfi.lv");
-        List<WebElement> articles = browser.findElements(ARTICLE);
-        Assertions.assertFalse(articles.isEmpty(), "There is no articles");
+        browser.get(HOME_PAGE);
+        List<WebElement> webArticles = browser.findElements(WEB_ARTICLE);
+        Assertions.assertFalse(webArticles.isEmpty(), "There is no articles");
 
-        List<String> firstArticles = new ArrayList<String>();
-        firstArticles.add("Поезд в аэропорт \"Рига\": билет будет дешевле, чем поездка на такси");
-        firstArticles.add("Трагический пожар в Риге: полиция начала уголовный процесс");
-        firstArticles.add("В Ригу вернулись блокираторы колес: все, что об этом надо знать");
-        firstArticles.add("Вике-Фрейберга о нацменьшинствах: если сравнить с Турцией, то у нас все блестяще");
-        firstArticles.add("Министр финансов Рейзниеце-Озола вышла из партии \"Латвии и Вентспилсу\"");
+        for (int i = 0; i < 5; i++) {
+            Assertions.assertEquals(articleTitle.get(i), webArticles.get(i).getText(),"This is not true article");
+        }
 
-        browser.get("http://m.rus.delfi.lv");
+        browser.get(MOB_HOME_PAGE);
         List<WebElement> mobArticles = browser.findElements(MOB_ARTICLE);
         Assertions.assertFalse(mobArticles.isEmpty(), "There is no articles");
 
-        List<String> firstMobArticles = new ArrayList<String>();
-        firstMobArticles.add("Поезд в аэропорт \"Рига\": билет будет дешевле, чем поездка на такси");
-        firstMobArticles.add("Трагический пожар в Риге: полиция начала уголовный процесс");
-        firstMobArticles.add("В Ригу вернулись блокираторы колес: все, что об этом надо знать");
-        firstMobArticles.add("Вике-Фрейберга о нацменьшинствах: если сравнить с Турцией, то у нас все блестяще");
-        firstMobArticles.add("Министр финансов Рейзниеце-Озола вышла из партии \"Латвии и Вентспилсу\"");
-
-        for (int i = 0; i < 5; i++){
-            Assertions.assertEquals(articles.get(i), mobArticles.get(i), "Articles are not equals");
+        for (int i = 0; i < 5; i++) {
+            Assertions.assertEquals(articleTitle.get(i), mobArticles.get(i).getText(),"This is not true article");
         }
+
+        for (int i = 0; i < 5; i++) {
+            Assertions.assertEquals(webArticles.get(i).getText(), mobArticles.get(i).getText(), "Articles Nr." + (i + 1) + " are not equals");
+        }
+
     }
+
     @AfterEach
-    public void closeBrowser(){
+    public void closeBrowser() {
         browser.close();
     }
 
 }
-
